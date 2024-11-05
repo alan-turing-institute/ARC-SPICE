e8f4ea97681e309c335672ab0dbba4118af73fd7
J-Dymond <jackdymond@me.com>
2024-11-05 10:47:08 +0000

    added document-level COMET to the test example script, also updated requirements.txt

--- scripts/variational_RTC_example.py
+++ scripts/variational_RTC_example.py
@@ -2,7 +2,10 @@
     An example use of the transcription, translation and summarisation pipeline.
 """
 
+from random import randint
+
 import torch
+from comet import download_model, load_from_checkpoint
 from torch.nn.functional import binary_cross_entropy
 
 from arc_spice.data.multieurlex_dataloader import MultiOneHot, load_multieurlex
@@ -17,10 +20,17 @@ MAX_LEN = 256
 def main(RTC_pars):
     lang_pair = {"source": "fr", "target": "en"}
     [train, _, _], metadata_params = load_multieurlex(level=1, lang_pair=lang_pair)
-    multi_onehot = MultiOneHot(metadata_params["n_classes"])
-
     row_iterator = iter(train)
-    test_row = next(row_iterator)
+    multi_onehot = MultiOneHot(metadata_params["n_classes"])
+    comet_model_pth = download_model(model="Unbabel/wmt22-comet-da")
+    # or for example:
+    # model_path = download_model("Unbabel/wmt22-comet-da")
+
+    # Load the model checkpoint:
+    comet_model = load_from_checkpoint(comet_model_pth)
+
+    for _ in range(0, randint(1, 25)):
+        test_row = next(row_iterator)
 
     class_labels = multi_onehot(test_row["class_labels"])
 
@@ -37,17 +47,31 @@ def main(RTC_pars):
     preds = torch.round(mean_scores)
     print(torch.mean((preds.float() == class_labels.float()).float()))
 
-    target_translation = test_row["target_text"]
+    source_text = test_row["target_text"]
+    target_text = test_row["target_text"]
     clean_translation = RTC.clean_output["translation"]["full_output"]
 
-    print(target_translation, clean_translation)
+    print(target_text, clean_translation)
 
     dl_bleu = get_bleu_score(
-        target_translation,
+        target_text,
         [clean_translation],
     )
 
-    print(f"Document-level BLEU: {dl_bleu}")
+    print(f"BLEU: {dl_bleu}")
+
+    comet_inp = [
+        {
+            "src": source_text,
+            "mt": clean_translation,
+            "ref": target_text,
+        }
+    ]
+
+    comet_output = comet_model.predict(comet_inp, batch_size=8, accelerator="cpu")
+    comet_scores = comet_output["scores"]
+
+    print(f"COMET: {comet_scores[0]}")
 
 
 if __name__ == "__main__":
