a04308840e1d835e4ae3d4540d693d8c1cf0b67b
J-Dymond <jackdymond@me.com>
2024-11-11 13:28:44 +0000

    cleaned up the inference example

--- scripts/variational_RTC_example.py
+++ scripts/variational_RTC_example.py
@@ -5,62 +5,60 @@
 from random import randint
 
 import torch
-from comet import download_model, load_from_checkpoint
 from torch.nn.functional import binary_cross_entropy
-from tqdm import tqdm
 
 from arc_spice.data.multieurlex_dataloader import MultiHot, load_multieurlex
-from arc_spice.eval.classification_error import (
-    MC_dropout_scores,
-    aggregate_score,
-    hamming_accuracy,
-)
-from arc_spice.eval.translation_error import get_bleu_score
+from arc_spice.eval.classification_error import hamming_accuracy
+from arc_spice.eval.translation_error import get_comet_model
 from arc_spice.variational_pipelines.RTC_variational_pipeline import (
     RTCVariationalPipeline,
 )
 
-MAX_LEN = 256
-
-
-def get_test_row(train_data):
-    return {
-        "source_text": "Le renard brun rapide a sauté par-dessus le chien paresseux.",
-        "target_text": "The quick brown fox jumped over the lazy dog.",
-        "class_labels": [0, 1],
-    }
-
 
 def main(RTC_pars):
-    # out = []
-    # for _ in range(5):
-    #     out.append(torch.rand(20).tolist())
-    # outs = MC_dropout_scores(out)
-
-    # print(torch.mean(outs["mutual_information"]))
-    # print(torch.mean(outs["variance"]))
-    # return None
-
+    # load data
     lang_pair = {"source": "fr", "target": "en"}
     [train, _, _], metadata_params = load_multieurlex(level=1, lang_pair=lang_pair)
     row_iterator = iter(train)
     multi_onehot = MultiHot(metadata_params["n_classes"])
-    comet_model_pth = download_model(model="Unbabel/wmt22-comet-da")
-
-    # Load the model checkpoint:
-    comet_model = load_from_checkpoint(comet_model_pth)
-
     test_row = next(row_iterator)
-
-    with open("temp/test_output.txt", "w") as text_file:
-        text_file.write("Purchase Amount: %s" % test_row["source_text"])
 
     class_labels = multi_onehot(test_row["class_labels"])
 
+    # load model
     RTC = RTCVariationalPipeline(RTC_pars, metadata_params)
     RTC.check_dropout()
+
+    # run inference
     RTC.variational_inference(test_row["source_text"])
 
+    # ### TRANSLATION ###
+    print("\nTranslation:")
+    source_text = test_row["target_text"]
+    target_text = test_row["target_text"]
+    clean_translation = RTC.clean_output["translation"]["full_output"]
+    print(
+        f"Semantic density: {RTC.var_output['translation']['weighted_semantic_density']}"
+    )
+
+    # load error model
+    comet_model = get_comet_model()
+    comet_inp = [
+        {
+            "src": source_text,
+            "mt": clean_translation,
+            "ref": target_text,
+        }
+    ]
+    # comet doesn't work on MPS
+    comet_device = "cuda" if torch.cuda.is_available() else "cpu"
+    comet_output = comet_model.predict(
+        comet_inp, batch_size=8, accelerator=comet_device
+    )
+    comet_scores = comet_output["scores"]
+    print(f"COMET: {comet_scores[0]}")
+
+    # ### CLASSIFICATION ###
     print("\nClassification:")
     mean_scores = RTC.var_output["classification"]["mean_scores"]
     print(f"BCE: {binary_cross_entropy(mean_scores.float(), class_labels.float())}")
@@ -69,35 +67,12 @@ def main(RTC_pars):
     print(f"hamming accuracy: {hamming_acc}")
 
     mean_entropy = torch.mean(RTC.var_output["classification"]["predicted_entropy"])
-    mean_std = torch.mean(RTC.var_output["classification"]["std_scores"])
     mean_variances = torch.mean(RTC.var_output["classification"]["var_scores"])
     mean_MI = torch.mean(RTC.var_output["classification"]["mutual_information"])
 
     print("Predictive entropy: " f"{mean_entropy}")
-    print("Standard deviation: " f"{mean_std}")
     print("MI (model uncertainty): " f"{mean_MI}")
     print("Variance (model uncertainty): " f"{mean_variances}")
-
-    print("\nTranslation:")
-    source_text = test_row["target_text"]
-    target_text = test_row["target_text"]
-    clean_translation = RTC.clean_output["translation"]["full_output"]
-    print(
-        f"Semantic density: {RTC.var_output['translation']['weighted_semantic_density']}"
-    )
-    comet_inp = [
-        {
-            "src": source_text,
-            "mt": clean_translation,
-            "ref": target_text,
-        }
-    ]
-
-    device = "cuda" if torch.cuda.is_available() else "cpu"
-
-    comet_output = comet_model.predict(comet_inp, batch_size=8, accelerator=device)
-    comet_scores = comet_output["scores"]
-    print(f"COMET: {comet_scores[0]}")
 
 
 if __name__ == "__main__":
--- src/arc_spice/eval/translation_error.py
+++ src/arc_spice/eval/translation_error.py
@@ -1,5 +1,13 @@
+from comet import download_model, load_from_checkpoint
 from torcheval.metrics.functional import bleu_score
 
 
 def get_bleu_score(target, translation):
     return bleu_score(target, translation, n_gram=4).item()
+
+
+def get_comet_model(model_path="Unbabel/wmt22-comet-da"):
+    # Load the model checkpoint:
+    comet_model_pth = download_model(model=model_path)
+    comet_model = load_from_checkpoint(comet_model_pth)
+    return comet_model
