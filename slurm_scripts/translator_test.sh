a9760cdbc5f58d12f75b68319748a25a2921098c
tbc <ziph8153@bask-pg-login03.cluster.baskerville.ac.uk>
2024-12-03 11:56:57 +0000

    Added a drop_length parameter, if passed it specifies a maximum character length of the first language passed to the dataset loader.

--- slurm_scripts/translator_test.sh
+++ slurm_scripts/translator_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH --account vjgo8416-spice
 #SBATCH --qos turing
-#SBATCH --job-name baskerville_pipeline_inference_test_translator_larger_bs
-#SBATCH --time 0-12:0:0
+#SBATCH --job-name baskerville_pipeline_inference_test_translator
+#SBATCH --time 0-24:0:0
 #SBATCH --nodes 1
 #SBATCH --gpus 1
 #SBATCH --output /bask/projects/v/vjgo8416-spice/ARC-SPICE/slurm_scripts/slurm_logs/baskerville_pipeline_inference_test_translator-%j.out
@@ -26,4 +26,4 @@ source /bask/projects/v/vjgo8416-spice/ARC-SPICE/env/bin/activate
 export HF_HOME="/bask/projects/v/vjgo8416-spice/hf_cache"
 
 # TODO: script uses relative path to project home so must be run from home, fix
-python scripts/single_component_inference.py /bask/projects/v/vjgo8416-spice/ARC-SPICE/config/RTC_configs/roberta-mt5-zero-shot.yaml /bask/projects/v/vjgo8416-spice/ARC-SPICE/config/data_configs/l1_fr_to_en.yaml 42 baskerville_pipeline_inference_test_large_bs translator
+python scripts/single_component_inference.py /bask/projects/v/vjgo8416-spice/ARC-SPICE/config/RTC_configs/roberta-mt5-zero-shot.yaml /bask/projects/v/vjgo8416-spice/ARC-SPICE/config/data_configs/l1_fr_to_en.yaml 42 baskerville_pipeline_inference_test translator
